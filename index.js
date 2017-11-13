bd85e49cdc3e9ddc6e9f130a42ce26bdc48ebf7d
Joe Dettmar <joedettmar@gmail.com>
2017-11-13 15:51:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (katzDeliLine.length === 0) {katzDeliLine.unshift(`${name}`); return `Welcome
 else {katzDeliLine.push(`${name}`); return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`};
 }
 
-function nowServing(katzDeliLine) {var newLine = katzDeliLine.push(); 
+function nowServing(katzDeliLine) {var newLine = (katzDeliLine.push(), return katzDeliLine); 
   if (katzDeliLine.length > 0) {
   return ("Currently serving katzDeliLine[0].")}
   else return `There is nobody waiting to be served!`;
