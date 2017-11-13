c155cac019edb777e86bdee88245af4bb2c11f7d
Joe Dettmar <joedettmar@gmail.com>
2017-11-13 16:03:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ else {katzDeliLine.push(`${name}`); return `Welcome, ${name}. You are number ${k
 
 function nowServing(katzDeliLine) {var newLine = katzDeliLine.slice(1, 3);
   if (katzDeliLine.length > 0) {
-  return 'Currently serving ${katzDeliLine[0]}.'}
+  return `Currently serving ${katzDeliLine[0]}.`}
   else return `There is nobody waiting to be served!`;
 }
 
